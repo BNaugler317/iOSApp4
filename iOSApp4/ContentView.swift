678cc2d6b167fd7d50710c6d6f6a004e2a4a12fc
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2026-06-25 10:26:27 -0300

    Initial Commit

---
+++ iOSApp4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp4
+//
+//  Created by Brandon Naugler on 2026-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp4/iOSApp4App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp4App.swift
+//  iOSApp4
+//
+//  Created by Brandon Naugler on 2026-06-25.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
